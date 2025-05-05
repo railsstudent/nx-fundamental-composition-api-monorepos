a7c2742f4172e5e66657c446e5f247727e27d061
Connie Leung <cleung334@gmail.com>
2025-05-05 16:07:57 +0800

    feat: completed day 2 lesson. list rendering

--- apps/fundamental-composition-api/src/app/NxWelcome.vue
+++ apps/fundamental-composition-api/src/app/NxWelcome.vue
@@ -2,15 +2,23 @@
 import { ref } from 'vue';
 
 const header = ref('Shopping List App')
+const items = ref([
+  { id: 1, label: '10 Apples' },
+  { id: 2, label: '20 Bananas' },
+  { id: 3, label: '5 Oranges' }
+]);
 
 </script>
 
 <template>
   <div class="wrapper">
     <div class="container">
-      <div id="welcome">
-        <h1>{{ header || 'Welcome' }}</h1>
-      </div>
+      <h1>{{ header || 'Welcome' }}</h1>
+      <ul>
+        <li v-for="{ id, label } in items" :key="id">
+          {{ label }}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
